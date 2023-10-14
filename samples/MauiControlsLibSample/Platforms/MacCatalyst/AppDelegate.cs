d051b3b50e5d18e7ecb85f9caa97f54241e29ef3
Sebastian Jensen <apps@tsjdev-apps.de>
2023-10-14 21:04:21 +0200

    Add sample project

---
+++ samples/MauiControlsLibSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiControlsLibSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
